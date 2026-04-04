ff6b7a34d1a40660eca9a6864e2416d761acf571
valkyraycho <razor0424@gmail.com>
2026-04-04 14:03:30 +0800

    feat: add built-in commands

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
-use std::io::{Write, stdin, stdout};
+use std::{
+    env::{self, current_dir, set_current_dir},
+    io::{Write, stdin, stdout},
+    process::Command,
+};
 
 fn main() {
     let mut input = String::new();
@@ -9,13 +13,27 @@ fn main() {
         if stdin().read_line(&mut input).expect("failed to read line") == 0 {
             break;
         };
-        let line = input.trim();
-        if line.is_empty() {
-            continue;
-        }
-        if line == "exit" {
-            break;
-        }
-        println!("your input: {}", line);
+
+        let mut elements = input.split_whitespace();
+        let (command, arguments) = (elements.next(), elements.collect::<Vec<&str>>());
+
+        match command {
+            None => continue,
+            Some("cd") => match arguments.first() {
+                None => set_current_dir(env::var("HOME").expect("HOME is not set"))
+                    .expect("failed to change directory"),
+                Some(path) => set_current_dir(path).expect("failed to change directory"),
+            },
+            Some("pwd") => {
+                println!("{}", current_dir().expect("failed to get cwd").display());
+            }
+            Some("exit") => break,
+            _ => {
+                Command::new(command.unwrap())
+                    .args(&arguments)
+                    .status()
+                    .expect("failed");
+            }
+        };
     }
 }
